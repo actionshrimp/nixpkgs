702813b51125dda01ea1c84921dafb24d651f79c
Dave Aitken <dave.aitken@gmail.com>
2022-12-17 14:25:32 +0000

    try the emacs-macport 'work' branch

--- pkgs/applications/editors/emacs/macport.nix
+++ pkgs/applications/editors/emacs/macport.nix
@@ -1,6 +1,6 @@
 import ./generic.nix rec {
   pname = "emacs-mac";
   version = "28.2";
-  macportVersion = "emacs-${version}-mac-9.1";
-  sha256 = "sha256-Ne2jQ2nVLNiQmnkkOXVc5AkLVkTpm8pFC7VNY2gQjPE=";
+  macportVersion = "work";
+  sha256 = "sha256-g+Ec8lKesq1NJs40gvFnY8oqH2hOzuK+xNRxHlAqgb0=";
 }
